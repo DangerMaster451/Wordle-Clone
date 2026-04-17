e9a95fae920951a74e43f29e3deba979a1c44745
DangerMaster451 <93455384+DangerMaster451@users.noreply.github.com>
2026-04-16 18:12:17 -0700

    basic structure

--- src/board.cpp
+++ src/board.cpp
@@ -1,2 +1,42 @@
 #include "raylib.h"
 #include "board.h"
+
+void Board::updateTile(int index, char value)
+{
+
+}
+
+bool Board::checkWin()
+{
+
+}
+
+void Board::render()
+{
+
+}
+
+int Tile::getIndex()
+{
+
+}
+
+char Tile::getStoredCharacter()
+{
+
+}
+
+void Tile::setIndex(int val)
+{
+
+}
+
+void Tile::setStoredCharacter(char val)
+{
+
+}
+
+void Tile::render(std::string answer)
+{
+
+}
--- src/board.h
+++ src/board.h
@@ -2,19 +2,27 @@
 #include<string>
 
 class Board {
+public:
+    Board() = default;
+    void updateTile(int index, char value);
+    bool checkWin();
+    void render();
 
+private:
+    Tile tiles[30];
+    std::string answer;
 };
 
 class Tile {
-private:
-    int index;
-    char storedCharacter;
 public:
+    Tile() = default;
     int getIndex();
     char getStoredCharacter();
     void setIndex(int val);
     void setStoredCharacter(char val);
-
     void render(std::string answer);
 
+private:
+    int index;
+    char storedCharacter;
 };
